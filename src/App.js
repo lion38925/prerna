67170f5f77b18323daea0d9fb96cf6459847e8de
PR <pr@Prajjwals-MacBook-Pro.local>
2021-05-23 20:42:38 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ export class HappyBirthday extends Component {
     this.state = {
       bannerClass: 'banner-hide',
       tempBanner: 'banner-show',
+      displayAbout: 'about-thoughts-hide',
       time: 0
     }
   }
   componentDidMount() {
     const date = new Date();
     const getDay = date.getDate();
-    if (getDay >= 24) {
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    if (getDay >= 24 && year >= 2021 && month >= 4) {
       setTimeout(() => {
         this.showBanner();
-      }, 5000);
+      }, 3000);
       this.setState({
         tempBanner: 'banner-hide'
 
@@ -30,6 +33,7 @@ export class HappyBirthday extends Component {
   showBanner = () => {
     this.setState({
       bannerClass: 'banner-show',
+      displayAbout: 'about-thoughts-show',
 
     })
   }
@@ -53,11 +57,17 @@ export class HappyBirthday extends Component {
           <div className={`temp-banner ${this.state.tempBanner}`}>Waiting for a Surprise
         </div>
         </div>
-
         <div className="App">
           <header className={`App-header ${this.state.bannerClass}`}>
             Happy Birthday Senorita
         </header>
+        </div>
+        <div className={`about-heading ${this.state.displayAbout}`}>
+          We love you peda
+          <p className='about-thoughts'>Happy Birthday my sweet baby senorita. Sada khush raho, aapka jeevan khushio bhara ho. Swast or khush raho <div> - Chotu</div></p>
+          <p className='about-thoughts'> <div> - Parmal</div></p>
+          <p className='about-thoughts'>Happy Birthday Gannu, puchhi... I wish you all the best on your Big Day and every day, may your life be full of joy, love and prosperity!
+          <div> - aman</div></p>
         </div>
       </React.Fragment>
     );
